0672cb6dce18942f82d94e89d6fda8154a1b1d89
Gabe Rodriguez <grodriguez@grodriguez-macbookpro.roam.corp.google.com>
2017-07-13 14:25:20 +0200

    added babel transpiling

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 
-function fibNaive (num) {
+export function naive (num) {
   if (num < 0 || typeof num !== "number") {
     throw new Error('Requires non-negative integer input')
   };
@@ -8,7 +8,7 @@ function fibNaive (num) {
   if (num === 0) return 0;
   if (num === 1) return 1;
 
-  return fibNaive(num-1) + fibNaive(num-2)
+  return naive(num-1) + naive(num-2)
 }
 
-module.exports = fibNaive;
+console.log(naive(10))
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,17 +1,17 @@
-const fibNaive = require('./fibonacci');
+import * as fib from './fibonacci.js';
 
 test('tests various known solutions in Fibonacci sequence', ()=>{
-  expect(fibNaive(0)).toBe(0);
-  expect(fibNaive(1)).toBe(1);
-  expect(fibNaive(2)).toBe(1);
-  expect(fibNaive(5)).toBe(5);
-  expect(fibNaive(10)).toBe(55);
-  expect(fibNaive(23)).toBe(28657);
-  expect(fibNaive(30)).toBe(832040);
+  expect(fib.naive(0)).toBe(0);
+  expect(fib.naive(1)).toBe(1);
+  expect(fib.naive(2)).toBe(1);
+  expect(fib.naive(5)).toBe(5);
+  expect(fib.naive(10)).toBe(55);
+  expect(fib.naive(23)).toBe(28657);
+  expect(fib.naive(30)).toBe(832040);
 });
 
 test('throws on bad inputs', ()=>{
-  expect(()=>{fibNaive(-1)}).toThrow();
-  expect(()=>{fibNaive('abc')}).toThrow();
-  expect(()=>{fibNaive()}).toThrow();
+  expect(()=>{fib.naive(-1)}).toThrow();
+  expect(()=>{fib.naive('abc')}).toThrow();
+  expect(()=>{fib.naive()}).toThrow();
 });
